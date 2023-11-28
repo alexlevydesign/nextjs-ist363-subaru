328e7f1ab05873cf719ad3bfab920b4159dd9a0d
Alex <alexlevy@columbus.rr.com>
2023-11-28 18:26:21 -0500

    Attendance 11/28

--- pages/vehicles/[id].js
+++ pages/vehicles/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/Layout';
 import { getVehicleBySlug, getAllVehicleSlugs } from '../../lib/api';
+import Image from 'next/image';
 
 //Waterfall
 // 1. getStaticPaths
@@ -32,9 +33,17 @@ export async function getStaticProps({params}) {
 }
 
 const SingleVehiclePage = ({vehicleData}) => {
-    const {title, slug} = vehicleData;
+    const {title, slug, featuredImage} = vehicleData;
     return <Layout>
         <h1>{title}</h1>
+        {featuredImage && //Conditional statement that ensures that the featured image has been uploaded.
+        <Image
+            src={featuredImage.node.sourceUrl}
+            alt={featuredImage.node.altText}
+            width={featuredImage.node.mediaDetails.width}
+            height={featuredImage.node.mediaDetails.height}
+        />
+        }
     </Layout>
 }
 
